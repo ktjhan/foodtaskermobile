73465e4d094234cf19a6de419f4291c2e623b0aa
Kavin Tjhan <tjhan.kavin@gmail.com>
2016-11-29 14:18:58 -0800

    Another Commit

---
+++ foodtaskermobile/CustomerMenuTableViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CustomerMenuTableViewController.swift
+//  foodtaskermobile
+//
+//  Created by Kavin Tjhan on 11/29/16.
+//  Copyright © 2016 Kavin Tjhan. All rights reserved.
+//
+
+import UIKit
+
+class CustomerMenuTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor(red: 0.19, green:0.18, blue: 0.31, alpha: 1.0)
+        
+    
+    }
+
+    
+}
---
+++ foodtaskermobile/MealDetailsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MealDetailsViewController.swift
+//  foodtaskermobile
+//
+//  Created by Kavin Tjhan on 11/29/16.
+//  Copyright © 2016 Kavin Tjhan. All rights reserved.
+//
+
+import UIKit
+
+class MealDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+   
+}
